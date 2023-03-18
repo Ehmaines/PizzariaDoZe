7d874e5d797ded33e8033ed121441f26dce77710
Eduardo Maines <edumaines27@gmail.com>
2023-03-17 22:26:32 -0300

    feat: adiciona icones na barra lateral

--- PizzariaDoZe/Properties/Resources.Designer.cs
+++ PizzariaDoZe/Properties/Resources.Designer.cs
@@ -73,9 +73,29 @@ namespace PizzariaDoZe.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap home {
+            get {
+                object obj = ResourceManager.GetObject("home", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PizzaMock {
             get {
                 object obj = ResourceManager.GetObject("PizzaMock", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap user {
+            get {
+                object obj = ResourceManager.GetObject("user", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
--- PizzariaDoZe/UserControls/BarraLateralUserControlForm.Designer.cs
+++ PizzariaDoZe/UserControls/BarraLateralUserControlForm.Designer.cs
@@ -28,27 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BarraLateralUserControlForm));
             panelBarraLateral = new Panel();
-            panelInicio = new Panel();
-            labelInicio = new Label();
-            panelFuncionario = new Panel();
-            labelFuncionarios = new Label();
-            panelClientes = new Panel();
-            labelClientes = new Label();
-            panelIngredientes = new Panel();
-            labelIngredientes = new Label();
             panelSabores = new Panel();
             labelSabores = new Label();
+            panelIngredientes = new Panel();
+            labelIngredientes = new Label();
+            panelClientes = new Panel();
+            labelClientes = new Label();
+            panelFuncionario = new Panel();
+            labelFuncionarios = new Label();
+            panelInicio = new Panel();
+            labelInicio = new Label();
+            pictureBoxUsuario = new PictureBox();
+            labelBemVindo = new Label();
+            labelGrupo = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            panelConfiguracoes = new Panel();
+            pictureBox6 = new PictureBox();
+            labelConfiguracoes = new Label();
             panelBarraLateral.SuspendLayout();
-            panelInicio.SuspendLayout();
-            panelFuncionario.SuspendLayout();
-            panelClientes.SuspendLayout();
-            panelIngredientes.SuspendLayout();
             panelSabores.SuspendLayout();
+            panelIngredientes.SuspendLayout();
+            panelClientes.SuspendLayout();
+            panelFuncionario.SuspendLayout();
+            panelInicio.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxUsuario).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            panelConfiguracoes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             SuspendLayout();
             // 
             // panelBarraLateral
             // 
+            panelBarraLateral.Controls.Add(panelConfiguracoes);
+            panelBarraLateral.Controls.Add(labelGrupo);
+            panelBarraLateral.Controls.Add(labelBemVindo);
+            panelBarraLateral.Controls.Add(pictureBoxUsuario);
             panelBarraLateral.Controls.Add(panelSabores);
             panelBarraLateral.Controls.Add(panelIngredientes);
             panelBarraLateral.Controls.Add(panelClientes);
@@ -59,89 +83,10 @@
             panelBarraLateral.Size = new Size(324, 1240);
             panelBarraLateral.TabIndex = 0;
             // 
-            // panelInicio
-            // 
-            panelInicio.BackColor = Color.FromArgb(163, 184, 247);
-            panelInicio.Controls.Add(labelInicio);
-            panelInicio.Location = new Point(3, 424);
-            panelInicio.Name = "panelInicio";
-            panelInicio.Size = new Size(318, 61);
-            panelInicio.TabIndex = 0;
-            // 
-            // labelInicio
-            // 
-            labelInicio.AutoSize = true;
-            labelInicio.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            labelInicio.ForeColor = Color.GhostWhite;
-            labelInicio.Location = new Point(3, 12);
-            labelInicio.Name = "labelInicio";
-            labelInicio.Size = new Size(83, 38);
-            labelInicio.TabIndex = 0;
-            labelInicio.Text = "Inicio";
-            // 
-            // panelFuncionario
-            // 
-            panelFuncionario.BackColor = Color.FromArgb(88, 95, 105);
-            panelFuncionario.Controls.Add(labelFuncionarios);
-            panelFuncionario.Location = new Point(3, 491);
-            panelFuncionario.Name = "panelFuncionario";
-            panelFuncionario.Size = new Size(318, 61);
-            panelFuncionario.TabIndex = 1;
-            // 
-            // labelFuncionarios
-            // 
-            labelFuncionarios.AutoSize = true;
-            labelFuncionarios.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            labelFuncionarios.ForeColor = Color.GhostWhite;
-            labelFuncionarios.Location = new Point(3, 11);
-            labelFuncionarios.Name = "labelFuncionarios";
-            labelFuncionarios.Size = new Size(174, 38);
-            labelFuncionarios.TabIndex = 0;
-            labelFuncionarios.Text = "Funcionários";
-            // 
-            // panelClientes
-            // 
-            panelClientes.BackColor = Color.FromArgb(88, 95, 105);
-            panelClientes.Controls.Add(labelClientes);
-            panelClientes.Location = new Point(3, 558);
-            panelClientes.Name = "panelClientes";
-            panelClientes.Size = new Size(318, 61);
-            panelClientes.TabIndex = 2;
-            // 
-            // labelClientes
-            // 
-            labelClientes.AutoSize = true;
-            labelClientes.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            labelClientes.ForeColor = Color.GhostWhite;
-            labelClientes.Location = new Point(0, 11);
-            labelClientes.Name = "labelClientes";
-            labelClientes.Size = new Size(115, 38);
-            labelClientes.TabIndex = 0;
-            labelClientes.Text = "Clientes";
-            // 
-            // panelIngredientes
-            // 
-            panelIngredientes.BackColor = Color.FromArgb(88, 95, 105);
-            panelIngredientes.Controls.Add(labelIngredientes);
-            panelIngredientes.Location = new Point(3, 625);
-            panelIngredientes.Name = "panelIngredientes";
-            panelIngredientes.Size = new Size(318, 61);
-            panelIngredientes.TabIndex = 3;
-            // 
-            // labelIngredientes
-            // 
-            labelIngredientes.AutoSize = true;
-            labelIngredientes.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            labelIngredientes.ForeColor = Color.GhostWhite;
-            labelIngredientes.Location = new Point(0, 11);
-            labelIngredientes.Name = "labelIngredientes";
-            labelIngredientes.Size = new Size(171, 38);
-            labelIngredientes.TabIndex = 0;
-            labelIngredientes.Text = "Ingredientes";
-            // 
             // panelSabores
             // 
             panelSabores.BackColor = Color.FromArgb(88, 95, 105);
+            panelSabores.Controls.Add(pictureBox4);
             panelSabores.Controls.Add(labelSabores);
             panelSabores.Location = new Point(3, 692);
             panelSabores.Name = "panelSabores";
@@ -153,11 +98,208 @@
             labelSabores.AutoSize = true;
             labelSabores.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
             labelSabores.ForeColor = Color.GhostWhite;
-            labelSabores.Location = new Point(3, 11);
+            labelSabores.Location = new Point(70, 9);
             labelSabores.Name = "labelSabores";
             labelSabores.Size = new Size(115, 38);
             labelSabores.TabIndex = 0;
             labelSabores.Text = "Sabores";
+            // 
+            // panelIngredientes
+            // 
+            panelIngredientes.BackColor = Color.FromArgb(88, 95, 105);
+            panelIngredientes.Controls.Add(pictureBox3);
+            panelIngredientes.Controls.Add(labelIngredientes);
+            panelIngredientes.Location = new Point(3, 625);
+            panelIngredientes.Name = "panelIngredientes";
+            panelIngredientes.Size = new Size(318, 61);
+            panelIngredientes.TabIndex = 3;
+            // 
+            // labelIngredientes
+            // 
+            labelIngredientes.AutoSize = true;
+            labelIngredientes.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            labelIngredientes.ForeColor = Color.GhostWhite;
+            labelIngredientes.Location = new Point(70, 11);
+            labelIngredientes.Name = "labelIngredientes";
+            labelIngredientes.Size = new Size(171, 38);
+            labelIngredientes.TabIndex = 0;
+            labelIngredientes.Text = "Ingredientes";
+            // 
+            // panelClientes
+            // 
+            panelClientes.BackColor = Color.FromArgb(88, 95, 105);
+            panelClientes.Controls.Add(pictureBox5);
+            panelClientes.Controls.Add(labelClientes);
+            panelClientes.Location = new Point(3, 558);
+            panelClientes.Name = "panelClientes";
+            panelClientes.Size = new Size(318, 61);
+            panelClientes.TabIndex = 2;
+            // 
+            // labelClientes
+            // 
+            labelClientes.AutoSize = true;
+            labelClientes.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            labelClientes.ForeColor = Color.GhostWhite;
+            labelClientes.Location = new Point(70, 10);
+            labelClientes.Name = "labelClientes";
+            labelClientes.Size = new Size(115, 38);
+            labelClientes.TabIndex = 0;
+            labelClientes.Text = "Clientes";
+            // 
+            // panelFuncionario
+            // 
+            panelFuncionario.BackColor = Color.FromArgb(88, 95, 105);
+            panelFuncionario.Controls.Add(pictureBox2);
+            panelFuncionario.Controls.Add(labelFuncionarios);
+            panelFuncionario.Location = new Point(3, 491);
+            panelFuncionario.Name = "panelFuncionario";
+            panelFuncionario.Size = new Size(318, 61);
+            panelFuncionario.TabIndex = 1;
+            // 
+            // labelFuncionarios
+            // 
+            labelFuncionarios.AutoSize = true;
+            labelFuncionarios.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            labelFuncionarios.ForeColor = Color.GhostWhite;
+            labelFuncionarios.Location = new Point(70, 11);
+            labelFuncionarios.Name = "labelFuncionarios";
+            labelFuncionarios.Size = new Size(174, 38);
+            labelFuncionarios.TabIndex = 0;
+            labelFuncionarios.Text = "Funcionários";
+            // 
+            // panelInicio
+            // 
+            panelInicio.BackColor = Color.FromArgb(163, 184, 247);
+            panelInicio.Controls.Add(pictureBox1);
+            panelInicio.Controls.Add(labelInicio);
+            panelInicio.Location = new Point(3, 424);
+            panelInicio.Name = "panelInicio";
+            panelInicio.Size = new Size(318, 61);
+            panelInicio.TabIndex = 0;
+            // 
+            // labelInicio
+            // 
+            labelInicio.AutoSize = true;
+            labelInicio.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            labelInicio.ForeColor = Color.GhostWhite;
+            labelInicio.Location = new Point(70, 12);
+            labelInicio.Name = "labelInicio";
+            labelInicio.Size = new Size(83, 38);
+            labelInicio.TabIndex = 0;
+            labelInicio.Text = "Inicio";
+            // 
+            // pictureBoxUsuario
+            // 
+            pictureBoxUsuario.Image = Properties.Resources.user;
+            pictureBoxUsuario.Location = new Point(114, 14);
+            pictureBoxUsuario.Name = "pictureBoxUsuario";
+            pictureBoxUsuario.Size = new Size(77, 75);
+            pictureBoxUsuario.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxUsuario.TabIndex = 5;
+            pictureBoxUsuario.TabStop = false;
+            // 
+            // labelBemVindo
+            // 
+            labelBemVindo.AutoSize = true;
+            labelBemVindo.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            labelBemVindo.ForeColor = Color.GhostWhite;
+            labelBemVindo.Location = new Point(3, 122);
+            labelBemVindo.Name = "labelBemVindo";
+            labelBemVindo.Size = new Size(131, 28);
+            labelBemVindo.TabIndex = 6;
+            labelBemVindo.Text = "Bem vindo(a),";
+            // 
+            // labelGrupo
+            // 
+            labelGrupo.AutoSize = true;
+            labelGrupo.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            labelGrupo.ForeColor = Color.GhostWhite;
+            labelGrupo.Location = new Point(3, 187);
+            labelGrupo.Name = "labelGrupo";
+            labelGrupo.Size = new Size(138, 28);
+            labelGrupo.TabIndex = 7;
+            labelGrupo.Text = "Administrador";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.home;
+            pictureBox1.Location = new Point(3, 8);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(45, 47);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(3, 10);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(45, 47);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(3, 11);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(45, 47);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(3, 9);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(45, 47);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(3, 7);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(45, 47);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            // 
+            // panelConfiguracoes
+            // 
+            panelConfiguracoes.BackColor = Color.FromArgb(88, 95, 105);
+            panelConfiguracoes.Controls.Add(pictureBox6);
+            panelConfiguracoes.Controls.Add(labelConfiguracoes);
+            panelConfiguracoes.Location = new Point(3, 759);
+            panelConfiguracoes.Name = "panelConfiguracoes";
+            panelConfiguracoes.Size = new Size(318, 61);
+            panelConfiguracoes.TabIndex = 5;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(3, 9);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(45, 47);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 4;
+            pictureBox6.TabStop = false;
+            // 
+            // labelConfiguracoes
+            // 
+            labelConfiguracoes.AutoSize = true;
+            labelConfiguracoes.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            labelConfiguracoes.ForeColor = Color.GhostWhite;
+            labelConfiguracoes.Location = new Point(70, 9);
+            labelConfiguracoes.Name = "labelConfiguracoes";
+            labelConfiguracoes.Size = new Size(194, 38);
+            labelConfiguracoes.TabIndex = 0;
+            labelConfiguracoes.Text = "Configurações";
             // 
             // BarraLateralUserControlForm
             // 
@@ -168,16 +310,26 @@
             Name = "BarraLateralUserControlForm";
             Size = new Size(327, 1243);
             panelBarraLateral.ResumeLayout(false);
-            panelInicio.ResumeLayout(false);
-            panelInicio.PerformLayout();
-            panelFuncionario.ResumeLayout(false);
-            panelFuncionario.PerformLayout();
-            panelClientes.ResumeLayout(false);
-            panelClientes.PerformLayout();
-            panelIngredientes.ResumeLayout(false);
-            panelIngredientes.PerformLayout();
+            panelBarraLateral.PerformLayout();
             panelSabores.ResumeLayout(false);
             panelSabores.PerformLayout();
+            panelIngredientes.ResumeLayout(false);
+            panelIngredientes.PerformLayout();
+            panelClientes.ResumeLayout(false);
+            panelClientes.PerformLayout();
+            panelFuncionario.ResumeLayout(false);
+            panelFuncionario.PerformLayout();
+            panelInicio.ResumeLayout(false);
+            panelInicio.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxUsuario).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            panelConfiguracoes.ResumeLayout(false);
+            panelConfiguracoes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ResumeLayout(false);
         }
 
@@ -194,5 +346,16 @@
         public Label labelIngredientes;
         public Panel panelInicio;
         public Label labelInicio;
+        public PictureBox pictureBoxUsuario;
+        public Label labelGrupo;
+        private Label labelBemVindo;
+        private PictureBox pictureBox1;
+        public Panel panelConfiguracoes;
+        private PictureBox pictureBox6;
+        public Label labelConfiguracoes;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox2;
     }
 }
