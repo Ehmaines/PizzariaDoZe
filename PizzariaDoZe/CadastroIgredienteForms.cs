8de9b144197726d00352d02dc02b715baa69e63e
eduardo.maines <edumaines27@gmail.com>
2023-05-08 10:11:44 -0300

    feat: adiciona conexão com banco e método de adicionar ingredientes no banco

--- PizzariaDoZe/CadastroIgredienteForms.cs
+++ PizzariaDoZe/CadastroIgredienteForms.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PizzariaDoZe_DAO;
+using System.Configuration;
 
 namespace PizzariaDoZe
 {
@@ -15,12 +17,20 @@ namespace PizzariaDoZe
     /// </summary>
     public partial class CadastroIngredienteForms : Form
     {
+        private readonly IngredienteDAO dao;
         /// <summary>
         /// Formulário de Cadastro de Ingredientes
         /// </summary>
         public CadastroIngredienteForms()
         {
             InitializeComponent();
+
+            string provider = ConfigurationManager.ConnectionStrings["BD"].ProviderName;
+            string strConnection = ConfigurationManager.ConnectionStrings["BD"].ConnectionString;
+
+            dao = new IngredienteDAO(provider, strConnection);
+
+            saveCloseUserControlForm1.btnSalvar.Click += SalvarIngrediente!;
             #region idioma/região interface - satellite assembly
             // com base no idioma/região escolhido pelo usuário,
             // ajusta as propriedades dos componentes da tela com base no conteúdo do arquivo resources
@@ -28,6 +38,25 @@ namespace PizzariaDoZe
             //ajuste manual de campos ou mensagens para o usuário que não puderam ser automatizadas acima
             this.Text = Properties.Resources.ResourceManager.GetString("txtTituloPrincipal");
             #endregion
+        }
+        private void SalvarIngrediente(object sender, EventArgs e)
+        {
+            var ingrediente = new Ingrediente()
+            {
+                IdIngrediente = 0,
+                Nome = txtNomeIngrediente.Text
+            };
+
+            try
+            {
+                dao.InserirDbProvider(ingrediente);
+
+                MessageBox.Show("Dados Inseridos com sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
--- PizzariaDoZe/Program.cs
+++ PizzariaDoZe/Program.cs
@@ -1,4 +1,5 @@
 using System.Configuration;
+using System.Data.Common;
 using System.Globalization;
 
 namespace PizzariaDoZe
@@ -16,6 +17,9 @@ namespace PizzariaDoZe
                 ConfigurationManager.AppSettings.Get("IdiomaRegiao") : ""; // en-US, es, pt-BR, etc
             Thread.CurrentThread.CurrentUICulture = new CultureInfo(auxIdiomaRegiao!);
             Thread.CurrentThread.CurrentCulture = new CultureInfo(auxIdiomaRegiao!);
+
+            DbProviderFactories.RegisterFactory("System.Data.SqlClient", System.Data.SqlClient.SqlClientFactory.Instance);
+            DbProviderFactories.RegisterFactory("MySql.Data.MySqlClient", MySql.Data.MySqlClient.MySqlClientFactory.Instance);
 
             ApplicationConfiguration.Initialize();
             Application.Run(new PaginaPrincipalForm());
